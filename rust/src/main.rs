bff5a2be2e638d2ab46454342377ea4754a32c9e
d86leader <d86leader@gmail.com>
2022-09-21 13:25:31 +0200

    don't remember what I was doing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,12 +4,45 @@ fn usage(execuable_name: std::string::String) {
     println!("Usage: {} FILENAME.ron\n\tParse RON file and measure time", execuable_name)
 }
 
+#[derive(Debug, serde::Deserialize, PartialEq, serde::Serialize)]
+#[serde(rename_all = "kebab-case")]
+enum MyEnumWithDashes {
+    ThisIsMyUnitVariant,
+    ThisIsMyTupleVariant(bool, i32),
+}
+
+#[derive(Debug, serde::Deserialize, PartialEq, serde::Serialize)]
+#[serde(rename_all = "kebab-case")]
+struct MyStructWithDashes {
+    my_enum: MyEnumWithDashes,
+    #[serde(rename = "2nd")]
+    my_enum2: MyEnumWithDashes,
+    will_be_renamed: u32,
+}
+
+fn roundtrip_ident_with_dash() {
+    let value = MyStructWithDashes {
+        my_enum: MyEnumWithDashes::ThisIsMyUnitVariant,
+        my_enum2: MyEnumWithDashes::ThisIsMyTupleVariant(false, -3),
+        will_be_renamed: 32,
+    };
+
+    let serial = ron::ser::to_string(&value).unwrap();
+
+    println!("Serialized: {}", serial);
+
+    let deserial = ron::de::from_str(&serial);
+
+    assert_eq!(Ok(value), deserial);
+}
+
 fn main() {
     let mut args = std::env::args();
     let executable = args.next().unwrap();
     let name = match args.next() {
         None => return usage(executable),
         Some(arg) if arg == "--help" => return usage(executable),
+        Some(arg) if arg == "test" => return roundtrip_ident_with_dash(),
         Some(name) => name,
     };
     let content = std::string::String::from_utf8(
